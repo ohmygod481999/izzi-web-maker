f9b15e3dd0abb447067f221f976b96d0ad85d289
longvb <vuongbaolong48@gmail.com>
2020-08-05 12:14:53 +0700

    update qurey

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,47 +1,86 @@
 import { gql } from "@apollo/client";
 import Config from "./config";
 
-const queries = {
-    article: (options) => {
-        if (!options) options = {};
-        const { limit, offset, order, sort } = options;
-        return gql`
-        query {
-            articles(
-                param: {
-                    type: 0
-                    keyword: ""
-                    limit: ${limit ? limit : 5}
-                    offset: ${offset ? offset : 0}
-                    order: ${order ? order : "desc"}
-                    sort: ${sort ? sort : "createdDate"}
-                    languageId: "${Config.languageId}"
-                    merchantId: "${Config.merchantId}"
-                }
-            ) {
-                totalCount
-                message
-                success
-                items {
-                    id
-                    name
-                    subDescription
-                    description
-                    categories {
-                        id
-                        name
-                    }
-                    images {
-                        id
-                        path
-                    }
-                    createdDate
-                    published
-                }
+export class QueryMulti {
+    constructor(name, params, items) {
+        this.name = name;
+        this.params = params;
+        this.items = items;
+    }
+
+    getQueryMulti = () => getQueryMulti(this.name, this.params, this.items);
+
+    getItems = (result) => result[this.name].items;
+}
+
+export class QuerySingle {
+    constructor(name, id, params, items) {
+        this.name = name;
+        this.id = id;
+        this.params = params;
+        this.items = items;
+    }
+
+    getQuerySingle = () =>
+        getQuerySingle(this.name, this.id, this.params, this.items);
+
+    getItem = (result) => result[this.name];
+}
+
+const getQueryMulti = (
+    name = "users",
+    params = {},
+    items = ["id", "name", "subDescription", "images {id, name}"]
+) => {
+    return gql`
+    query {
+        ${name}(
+            param: {
+                keyword: "${params.keyword ? params.keyword : ""}", 
+                limit: ${params.limit ? params.limit : 5}, 
+                offset: ${params.offset ? params.offset : 0}, 
+                order: "${params.order ? params.order : "desc"}", 
+                sort: "${params.sort ? params.sort : "createdDate"}", 
+                languageId: "${
+                    params.languageId ? params.languageId : Config.languageId
+                }", 
+                merchantId: "${
+                    params.merchantId ? params.merchantId : Config.merchantId
+                }"
+            }
+        ) {
+            totalCount
+            message
+            success
+            items {
+                ${items.reduce((acc, cur) => acc + "\n" + cur, "")}
             }
         }
-    `;
-    },
+    }
+`;
 };
 
-export default queries;
+const getQuerySingle = (
+    name = "article",
+    id,
+    params = {},
+    items = ["id", "name", "subDescription", "images {id, name}", "description"]
+) => {
+    return gql`
+    query {
+        ${name}(
+            param: {
+                id:"${id}",
+                languageId: "${
+                    params.languageId ? params.languageId : Config.languageId
+                }", 
+                merchantId: "${
+                    params.merchantId ? params.merchantId : Config.merchantId
+                }"
+            }
+        ) {
+            ${items.reduce((acc, cur) => acc + "\n" + cur, "")}
+        }
+    }
+`;
+};
